b91aa304143b725adcdb5282e34f759a127946f8
475Fatema <66346391+475Fatema@users.noreply.github.com>
2020-07-06 23:46:34 +0300

    تم
    لم اكمل الواجب

---
+++ grade.js
@@ -0,0 +1,64 @@
+// let number =[
+//     {
+//      name:"quiz",
+//      grade:10,
+//      },
+
+//     {
+//      name:"midterm",
+//       grade:25,
+//     },
+
+//        {
+//      name:"final test",
+//      grade:50,
+//     },
+
+//        {
+//      name:"oral",
+//      grade:15,
+//     },
+// ];
+//let number=[90&&100];
+
+
+let result=[];
+
+function loop(){
+let counter = 0;
+while  (counter==100,90) {
+  console.log("A")
+  document.getElementsByClassName("numberByid") = result;
+  let result=[];
+}
+}
+
+// أكبر من أو يساوي 80      B
+// أكبر من أو يساوي 70      C
+// أكبر من أو يساوي 60      D
+// عدا ذلك                  F 
+
+let counter=0;
+
+if (counter>=80) {
+  console.log("B")
+} else if (counter>=70) {
+ console.log("C")
+} else if (counter>=60) {
+  let resulte_letter 
+  document.getElementsByClassName("resulte_letter").write("D");
+  console.log("D")
+}else{
+  document.getElementsByClassName("resulte_letter")=Text;
+  console.log("F")
+}
+
+
+
+
+
+
+
+
+
+
